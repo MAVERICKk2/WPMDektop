e9778c354a7739545984d80a255392e86c8b140e
MAVERICKk2 <MAVERICKk2@users.noreply.github.com>
2018-01-30 16:43:44 -0600

    improvements

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
@@ -14,7 +15,7 @@ let mainWindow;
 function createWindow() {
     //Create the browser window.
     mainWindow = new BrowserWindow({width: 1150, height: 700, icon: "img/snake.jpg"});
-    mainWindow.setMenu(null);
+    mainWindow.setMenu(false);
 
     // load the index.html of the app
     mainWindow.loadURL(url.format({
@@ -24,7 +25,7 @@ function createWindow() {
     }));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -33,12 +34,12 @@ function createWindow() {
     // when you should delete the corresponding element.
     mainWindow = null
   })  
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -47,7 +48,7 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
       app.quit()
     }
-  })
+  });
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -55,7 +56,4 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})  
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.  
+});
